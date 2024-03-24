5c8f9973dde7688512dc8b51bdb0cffc17dfd004
lute47lillo <lutelillocs47@gmail.com>
2024-03-24 19:55:34 -0400

    Part 3 + snippet Part 4

--- filter_ssvep_data.py
+++ filter_ssvep_data.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import bci_filtering_plot as bfp
 
-def make_bandpass_filter(low_cutoff, high_cutoff, filter_order, fs, filter_type="hann"):
+def make_bandpass_filter(low_cutoff, high_cutoff, filter_order=10, fs=1000, filter_type="hann"):
     
     low_cutoff_adjusted = low_cutoff
     high_cutoff_adjusted = high_cutoff
@@ -41,9 +41,65 @@ def make_bandpass_filter(low_cutoff, high_cutoff, filter_order, fs, filter_type=
     axis[1].plot(frequency_response, bfp.convert_to_db(H_f), label="frequency response")
     axis[1].set_title("frequency response")
     axis[1].set_xlabel('frequency (Hz)')
+    axis[1].set_xlim(0, 60)
     axis[1].set_ylabel('amplitude (dB)')
     
-    plt.savefig(f"plots/{filter_type}_filter_{low_cutoff}-{high_cutoff}Hz_order{filter_order}.png")
+    # plt.savefig(f"plots/Limit_{filter_type}_filter_{low_cutoff}-{high_cutoff}Hz_order{filter_order}.png")
     
     return filter_coefficients
 
+
+def filter_data(data, b):
+    """
+        Definition:
+        ----------
+            Apply filter through the function scipy.signal.filtfilt() to the raw data.
+        
+        Parameters:
+        ----------
+            - data (dict): the raw data dictionary.
+            - b (array | Shape (n_filter_order)): the filter coefficients produced in function make_bandpass_filter().
+            
+        Returns:
+        ----------
+            - filtered_data: data dictionary after having applied the filter forwards
+            and backwards in time to each channel in the raw data.
+    """
+    eeg = data['eeg']       
+
+    filter_data = signal.filtfilt(b, a=1, x=eeg)
+    
+    return filter_data
+
+def get_envelope(data, filtered_data, channel_to_plot=None, ssvep_frequency=None):
+    """
+        Definition:
+        ----------
+            
+        
+        Parameters:
+        ----------
+            - data (dict): the raw data dictionary.
+            - filtered_data (array | Shape: (n_channels, eeg_data_points)): the filtered data.
+            - channel_to_plot (str): which channel to plot.
+            - ssvep_frequency (str): the SSVEP frequency being isolated.
+            
+        Returns:
+        ----------
+            - envelope (array | Shape (n_channels, n_time_points)): Amplitude of oscillations
+    
+    """
+    if channel_to_plot is None:
+        # Create new plot
+        pass
+    
+    
+    
+    if ssvep_frequency is None:
+        plt.savefig(f"plots/unknown_freq.png")
+    # If the ssvep_frequency input is None, your plot’s title should state that the frequency being 
+    # isolated is unknown. The function should return the amplitude of oscillations (on every channel at 
+    # every time point) in an array called envelope.
+
+    envelope = []
+    return envelope
--- test_filter_ssvep_data.py
+++ test_filter_ssvep_data.py
@@ -3,7 +3,7 @@ from import_ssvep_data import plot_raw_data as plot
 from import_ssvep_data import epoch_ssvep_data
 import numpy as np
 from import_ssvep_data import get_frequency_spectrum
-from filter_ssvep_data import make_bandpass_filter
+from filter_ssvep_data import make_bandpass_filter, filter_data
 
 subject = 1
 data_directory = "SSVEP_data"
@@ -11,8 +11,11 @@ data_directory = "SSVEP_data"
 # Part 1
 data = load(subject, data_directory)
 
-
 # Part 2
-
 filter_coeff_1 = make_bandpass_filter(13, 15, filter_order=1000, fs=1000)
 filter_coeff_2 = make_bandpass_filter(12, 14, filter_order=1000, fs=1000)
+
+
+# Part 3
+filtered_data_1 = filter_data(data, filter_coeff_1)
+filtered_data_2 = filter_data(data, filter_coeff_2)

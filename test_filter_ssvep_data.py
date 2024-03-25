3478cca85e1e0b803f9bea099209240158a8d6d5
marajadeskywalker <89876990+marajadeskywalker@users.noreply.github.com>
2024-03-25 18:19:47 -0400

    Update test_filter_ssvep_data.py

--- test_filter_ssvep_data.py
+++ test_filter_ssvep_data.py
@@ -3,10 +3,23 @@ from import_ssvep_data import plot_raw_data as plot
 from import_ssvep_data import epoch_ssvep_data
 import numpy as np
 from import_ssvep_data import get_frequency_spectrum
+from filter_ssvep_data import make_bandpass_filter, filter_data, get_envelope
 
 subject = 1
-data_directory = '/Users/Max/Documents/BCIs/project-3/Lab-3/SsvepData'
+data_directory = "SSVEP_data"
 
 # Part 1
 data = load(subject, data_directory)
-print(data['time'])
+
+# Part 2
+filter_coeff_1 = make_bandpass_filter(13, 15, filter_order=1000, fs=1000)
+filter_coeff_2 = make_bandpass_filter(12, 14, filter_order=1000, fs=1000)
+
+
+# Part 3
+filtered_data_1 = filter_data(data, filter_coeff_1)
+filtered_data_2 = filter_data(data, filter_coeff_2)
+
+# Part 4
+envelope_1 = get_envelope(data, filtered_data_1, "3", None)
+envelope_2 = get_envelope(data, filtered_data_2, "5", None)
